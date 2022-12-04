6e9c5e307fde1d9cef4b1cea73d0b334ab894f54
genmla <gmmladenka@gmail.com>
2022-12-04 17:12:18 -0600

    add JS files for roles and package.json content

---
+++ index.js
@@ -0,0 +1,7 @@
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager')
+const Intern = require('./lib/intern')
+const Engineer = require('./lib/engineer')
+
+const Grace = new Employee (Grace, 'gra123', 'grace@gmail.com')
+getRole(Grace)
